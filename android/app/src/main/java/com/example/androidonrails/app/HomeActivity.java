0b219871d827eec692b6b91270b6285d79ae69b9
Derrick Zhang <robot@zillou.me>
2014-05-09 07:01:09 +0800

    Add android project

---
+++ android/app/src/main/java/com/example/androidonrails/app/HomeActivity.java
@@ -0,0 +1,86 @@
+package com.example.androidonrails.app;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.savagelook.android.UrlJsonAsyncTask;
+
+
+public class HomeActivity extends ActionBarActivity {
+    private static final String TASKS_URL = "http://0.0.0.0:3000/api/v1/tasks.json";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        loadTasksFromAPI(TASKS_URL);
+    }
+    private void loadTasksFromAPI(String url) {
+        GetTasksTask getTasksTask = new GetTasksTask(HomeActivity.this);
+        getTasksTask.setMessageLoading("Loading tasks...");
+        getTasksTask.execute(url);
+    }
+
+    private class GetTasksTask extends UrlJsonAsyncTask {
+        public GetTasksTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                JSONArray jsonTasks = json.getJSONObject("data").getJSONArray("tasks");
+                int length = jsonTasks.length();
+                List<String> tasksTitles = new ArrayList<String>(length);
+
+                for (int i = 0; i < length; i++) {
+                    tasksTitles.add(jsonTasks.getJSONObject(i).getString("title"));
+                }
+
+                ListView tasksListView = (ListView) findViewById (R.id.tasks_list_view);
+                if (tasksListView != null) {
+                    tasksListView.setAdapter(new ArrayAdapter<String>(HomeActivity.this,
+                            android.R.layout.simple_list_item_1, tasksTitles));
+                }
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                loadTasksFromAPI(TASKS_URL);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
