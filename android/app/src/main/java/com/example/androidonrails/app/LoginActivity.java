4fa42bfb02582fb7c063d70cbacfcb3be8969dfc
Derrick Zhang <derrick.zhang@welab.co>
2014-05-09 09:34:44 +0800

    update

--- android/app/src/main/java/com/example/androidonrails/app/LoginActivity.java
+++ android/app/src/main/java/com/example/androidonrails/app/LoginActivity.java
@@ -6,8 +6,6 @@ import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
--- rails/app/controllers/api/v1/tasks_controller.rb
+++ rails/app/controllers/api/v1/tasks_controller.rb
@@ -2,11 +2,6 @@ class Api::V1::TasksController < ApplicationController
   skip_before_filter :verify_authenticity_token,
     :if => Proc.new { |c| c.request.format == 'application/json' }
 
-
-  # Just skip the authentication for now
-  # before_filter :authenticate_user!
-  skip_before_filter :authenticate_user!
-
   respond_to :json
 
   def index
